36f4a1d203cb0aba3bb86fc808e9a3af81b389f0
lucaspari <lucaspariii@gmail.com>
2023-07-11 14:13:34 -0300

    feat: changed the route in order to fulfill requirements of API

--- src/main/java/br/com/banco/controllers/TransferenciaController.java
+++ src/main/java/br/com/banco/controllers/TransferenciaController.java
@@ -1,11 +1,14 @@
 package br.com.banco.controllers;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.banco.models.Transferencia;
@@ -19,8 +22,25 @@ import lombok.AllArgsConstructor;
 public class TransferenciaController {
     private TransferenciaService transferenciaService;
 
-    @GetMapping("/listar")
-    public ResponseEntity<List<Transferencia>> listar() {
-        return ResponseEntity.ok(transferenciaService.findAllTransferencias());
+    @GetMapping("/listar/search/")
+    public ResponseEntity<List<Transferencia>> listar(
+        @RequestParam(value = "dateTime", required = false)
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDateTime dateTime,
+        @RequestParam(value = "nome", required = false)
+        String nome) {
+    List<Transferencia> transferencias;
+
+    if (dateTime != null && nome != null) {
+        transferencias = transferenciaService.findAllTransferenciaByContaNomeResponsavelAndData(nome, dateTime);
+    } else if (dateTime != null) {
+        transferencias = transferenciaService.findAllTransferenciaByData(dateTime);
+    } else if (nome != null) {
+        transferencias = transferenciaService.findAllTransferenciaByContaNomeResponsavel(nome);
+    } else {
+        transferencias = transferenciaService.findAllTransferencias();
     }
+
+    return ResponseEntity.ok(transferencias);
+}
 }
