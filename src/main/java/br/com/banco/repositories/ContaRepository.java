8539e81c28491fd635531dccf2a00a36eea52c0c
lucaspari <lucaspariii@gmail.com>
2023-07-11 13:38:58 -0300

    feat: added mutiple services to the API

--- src/main/java/br/com/banco/repositories/ContaRepository.java
+++ src/main/java/br/com/banco/repositories/ContaRepository.java
@@ -2,8 +2,8 @@ package br.com.banco.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.com.banco.models.Conta;
 
+import br.com.banco.models.Conta;
 public interface ContaRepository extends JpaRepository<Conta, Long> {
 
 }
--- src/main/java/br/com/banco/repositories/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repositories/TransferenciaRepository.java
@@ -1,9 +1,18 @@
 package br.com.banco.repositories;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.banco.models.Transferencia;
 
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
+
+    List<Transferencia> findAllByDataTransferencia(LocalDateTime data);
+
+    List<Transferencia> findAllByContaNomeResponsavel(String nome);
+
+    List<Transferencia> findAllByContaNomeResponsavelAndDataTransferencia(String nome, LocalDateTime data);
 
 }
