4701ef77c1b93ac0707eca597be97a2054aac84f
lucaspari <lucaspariii@gmail.com>
2023-07-11 12:27:08 -0300

    feat: Added models Conta and Transferencia along with their dependencies

---
+++ src/main/java/br/com/banco/model/Transferencia.java
@@ -0,0 +1,41 @@
+package br.com.banco.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "transferencia")
+public class Transferencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime dataTransferencia;
+
+    private BigDecimal valor;
+
+    private String tipo;
+
+    private String nomeOperadorTransacao;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_id", nullable = false)
+    private Conta conta;
+
+}
